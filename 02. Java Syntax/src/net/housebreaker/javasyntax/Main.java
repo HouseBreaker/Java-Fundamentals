6a9f26f18152d08f616dfcbf58c58ddff365ca85
Housey <housebreaker1@gmail.com>
2015-10-17 18:43:40 +0300

    add java syntax homework

---
+++ 02. Java Syntax/src/net/housebreaker/javasyntax/Main.java
@@ -0,0 +1,9 @@
+package net.housebreaker.javasyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        p01_RectangleArea.main();
+        
+    }
+}
---
+++ 02. Java Syntax/src/net/housebreaker/javasyntax/p01_RectangleArea.java
@@ -0,0 +1,7 @@
+package net.housebreaker.javasyntax;
+
+public class p01_RectangleArea {
+    public static void main(){
+
+    }
+}
