3311fc75e19c004455a56ccc976dd00c91ce7722
Housey <housebreaker1@gmail.com>
2015-10-28 16:56:59 +0200

    add oop in java homework

---
+++ 05. OOP in Java/src/p01_Geometry/GeometryMain.java
@@ -0,0 +1,8 @@
+package p01_Geometry;
+
+public class GeometryMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 05. OOP in Java/src/p01_Geometry/Shape.java
@@ -0,0 +1,8 @@
+package p01_Geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shape {
+	List<Vertex> vertices = new ArrayList<>();
+}
