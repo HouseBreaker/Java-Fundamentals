4ec9d96663510c2ba7c358375ddf380cb8816862
Housey <housebreaker1@gmail.com>
2015-11-01 00:42:46 +0200

    fix commit reformat error

--- 05. OOP in Java/src/p03_GandalfsStash/GandalfsStash.java
+++ 05. OOP in Java/src/p03_GandalfsStash/GandalfsStash.java
@@ -20,41 +20,35 @@ public class GandalfsStash {
 			foodList.add(matcher.group().toLowerCase());
 		}
 
-		for (String food : foodList) {
-			switch (food) {
-				case "cram":
-					mood += 2;
-					break;
-				case "lembas":
-					mood += 3;
-					break;
-				case "apple":
-					mood += 1;
-					break;
-				case "melon":
-					mood += 1;
-					break;
-				case "honeycake":
-					mood += 5;
-					break;
-				case "mushrooms":
-					mood -= 10;
-					break;
-				default:
-					mood -= 1;
-					break;
+		for (String food : foodList){
+			switch (food)
+			{
+				case "cram":		mood += 2;	break;
+				case "lembas":		mood += 3;	break;
+				case "apple":		mood += 1;	break;
+				case "melon":		mood += 1;	break;
+				case "honeycake":	mood += 5;	break;
+				case "mushrooms":	mood -= 10;	break;
+				default:			mood -= 1;	break;
 			}
 		}
 
 		System.out.println(mood);
 
-		if (mood < -5) {
+		if (mood < -5)
+		{
 			System.out.println("Angry");
-		} else if ((mood >= -5) && (mood < 0)) {
+		}
+		else if ((mood >= -5) && (mood < 0))
+		{
 			System.out.println("Sad");
-		} else if ((mood <= 15) && (mood >= 0)) {
+		}
+		else if ((mood <= 15) && (mood >= 0))
+		{
 			System.out.println("Happy");
-		} else if (mood > 15) {
+		}
+		else if (mood > 15)
+		{
 			System.out.println("Special JavaScript mood");
 		}
 	}
