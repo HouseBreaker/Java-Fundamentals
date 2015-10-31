5def6089d5ab56d39cd5e3b2c9bf86b5e8a81f0d
Housey <housebreaker1@gmail.com>
2015-11-01 01:35:36 +0200

    add 4th problem

---
+++ 05. OOP in Java/src/p04_LettersChangeNumbers/LettersChangeNumbers.java
@@ -0,0 +1,44 @@
+package p04_LettersChangeNumbers;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LettersChangeNumbers {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+
+		ArrayList<String> listOfStrings = new ArrayList<>();
+
+		Matcher matcher = Pattern.compile("([^\\W_]+)(?=\\W*)").matcher(input);
+
+		while (matcher.find()) {
+			listOfStrings.add(matcher.group());
+		}
+
+		double total = 0;
+		
+		for (String word : listOfStrings) {
+			double theNumber = Double.parseDouble(word.substring(1, word.length() - 1));
+
+			char[] chars = {word.charAt(0), word.charAt(word.length() - 1)};
+
+			if (chars[0] >= 'A' && chars[0] <= 'Z') {
+				theNumber /= chars[0] - 'A' + 1;
+			} else if (chars[0] >= 'a' && chars[0] <= 'z') {
+				theNumber *= chars[0] - 'a' + 1;
+			}
+
+			if (chars[1] >= 'A' && chars[1] <= 'Z') {
+				theNumber -= chars[1] - 'A' + 1;
+			} else if (chars[1] >= 'a' && chars[0] <= 'z') {
+				theNumber += chars[1] - 'a' + 1;
+			}
+
+			total += theNumber;
+		}
+		System.out.printf("%.2f", total);
+	}
+}
